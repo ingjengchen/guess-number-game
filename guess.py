d61ab93b34c6cef708d6a569a3fabbbf58dee602
Evan Chen <chikakamarimba@hotmail.com>
2021-05-31 22:33:40 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,13 +1,17 @@
 import random
+start = input('請決定隨機數字範圍的開始值: ')
+end = input('請決定隨機數字範圍的結束值: ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1 # count = count + 1
 	n = input('請猜數字: ')
 	n = int(n)
 	if n == r:
-		print('恭喜答對')
+		print('恭喜答對! 你共猜了', count, '次')
 		break
 	elif n < r:
 		print('比', n, '大')
